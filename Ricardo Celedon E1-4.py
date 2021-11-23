313cd755599f2ed1b5f792067fd365106097c6f1
rjceledon <85322110+rjceledon@users.noreply.github.com>
2021-11-23 11:20:41 -0500

    Delete Ricardo Celedon E1-4.py
    Too simple

--- Ricardo Celedon E1-4.py
+++
@@ -1,5 +0,0 @@
-#Realizado por Ing. Ricardo Celedon
-w1 = input("Por favor ingrese una palabra: ")
-w2 = input("Ahora ingrese otra palabra: ")
-concat = (w1 + " " + w2)
-print("El resultado de la concatenacion es:", concat)
