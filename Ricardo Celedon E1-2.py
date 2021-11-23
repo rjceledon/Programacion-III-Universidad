35b81f4696fbbd25778a90855125ed57c5775759
rjceledon <85322110+rjceledon@users.noreply.github.com>
2021-11-23 11:20:08 -0500

    Delete Ricardo Celedon E1-2.py
    Too simple

--- Ricardo Celedon E1-2.py
+++
@@ -1,5 +0,0 @@
-#Realizado por Ing. Ricardo Celedon
-dec = float(input("Por favor ingrese un numero decimal: "))
-ent = int(input("Ahora ingrese un numero entero: "))
-suma = dec + ent
-print("El resultado de la suma es:", suma)
